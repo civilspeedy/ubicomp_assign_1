3aacbcf91983f5761bfac08bbb0b5fc6972e6b54
Charlie <106312897+civilspeedy@users.noreply.github.com>
2023-10-13 17:07:29 +0100

    Testing out nav bar

--- assignment_1/App.js
+++ assignment_1/App.js
@@ -1,15 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
 import HomeSplash from './HomeSplash';
-import { View, StyleSheet, Pressable, Text} from 'react-native';
+import { View, StyleSheet} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Navigation from './Navigation'
+
+//ChatGPT was asked for basic code for a 'bottom nav bar', fragments from responce will be highlighted
+
+const Tab = createBottomTabNavigator() // from ChatGPT
 
 export default function App() {
   return (
     <View style={homeStyle.container}> 
-      <HomeSplash/>
+
       <View style={homeStyle.navBar}>
-        <Pressable><Text>Test</Text></Pressable>
-        <Pressable><Text>Test</Text></Pressable>
-        <Pressable><Text>Test</Text></Pressable>
-        <Pressable><Text>Test</Text></Pressable>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name='Home' component={HomeSplash}/>
+          </Tab.Navigator>
+        </NavigationContainer>
       </View>
     </View>
   )
@@ -25,5 +33,5 @@ const homeStyle= StyleSheet.create({
   navBar: {
     flexDirection: 'row',
     backgroundColor: 'lightgreen',
-    }
+    },
 })
