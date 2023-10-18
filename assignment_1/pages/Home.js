f3248e1df2c61e8386df2784afdf35e526374ab8
Charlie <106312897+civilspeedy@users.noreply.github.com>
2023-10-18 15:39:34 +0100

    Moved to scroll View although something keeps causing the card to disappear

--- assignment_1/pages/Home.js
+++ assignment_1/pages/Home.js
@@ -1,44 +1,37 @@
 import React from "react"; 
-import { Text, View, StyleSheet, Dimensions } from "react-native";
-import Carousel from "react-native-snap-carousel";
+import { Text, View, StyleSheet, Dimensions, SafeAreaView } from "react-native";
+import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
 
-const carouselCards = [
-    {id: 1, name: "nearby"},
-    {id: 2, name: "nearby"},
-    {id: 3, name: "nearby"},
+const cardData = [
+    {id: 1, locaton: "London", length: "2 Miles"}
 ]
 
-const sliderWidth = Dimensions.get("window").width + 30;
-const cardWidth = Math.round(sliderWidth * 0.8);
-
-const renderItem = ({item}) => {
-    return(
-        <View style={styles.card}>
-            <Text>{item.name}</Text>
-        </View>
-    )
+function generateCards(){
+    for (const card in cardData){
+        
+    }
 }
+
 
 export default function Home(){
     return (
-        <View>
-            <Carousel 
-            data={carouselCards}
-            renderItem={renderItem}
-            sliderWidth={sliderWidth}
-            itemWidth={cardWidth}/>
-        </View>
-    )
+        <GestureHandlerRootView>
+            <SafeAreaView>
+                <ScrollView>
+                    <View style={styles.card}/>
+                </ScrollView>
+            </SafeAreaView>
+        </GestureHandlerRootView>
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
-
+    safeAreaView:{
+        flex:1,
     },
 
     card:{
-        borderWidth: 1,
-        padding: 20,
+        flex: 1,
         alignItems: 'center',
         backgroundColor: 'grey',
     }
